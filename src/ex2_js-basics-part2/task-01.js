fe5bddf51b2101695610bb32035b7a73be65d774
Anton Kanopkin <39925338+kanstudio@users.noreply.github.com>
2018-10-03 17:09:06 +0200

    Update task-01.js

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -2,7 +2,7 @@
 
 function detectType(x) {
   if(typeof x === 'number' || typeof x === 'string') {return typeof x;}
-  else {return 'undefined';}
+  return 'undefined';
 }
 
 module.exports = detectType;
