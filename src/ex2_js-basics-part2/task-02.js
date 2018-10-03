0804352c4805373ac45e5e85c5b9c8121fb8d5fb
Anton Kanopkin <39925338+kanstudio@users.noreply.github.com>
2018-10-03 17:10:07 +0200

    Update task-02.js

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function viewArray(arr) {
+  var i;
   console.log('Обзор массива:');
-  for(var i=0; i<arr.length; i++) {
+  for(i=0; i<arr.length; i++) {
     console.log('[' + i + '] => ' + arr[i]);
   }
   console.log('Всего элементов: ' + arr.length);
