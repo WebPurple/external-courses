a9666d06738b36a5a846394a3d60bf72bc677ce3
Anton Kanopkin <39925338+kanstudio@users.noreply.github.com>
2018-10-03 13:37:52 +0200

    Task 04 is ready

---
+++ src/ex2_js-basics-part2/task-04.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function isAllElementsEqual(arr) {
+  switch(arr.length) {
+    case 0: return false;
+    case 1: return true;
+    default:
+      var i = 0;
+      while(i<arr.length && arr[i++] === arr[i]);
+      if(i === arr.length) {return true;}
+      else {return false;}
+  }
+}
