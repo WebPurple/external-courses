7fab4e236a537c5672e17e43d7fbd71cd589bcfd
Anton Kanopkin <39925338+kanstudio@users.noreply.github.com>
2018-10-03 17:14:26 +0200

    Update task-06.js

--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -2,7 +2,7 @@
 
 function isSimpleNumber(x) {
   var i;
-  if(typeof x !== 'number' || isNaN(x) || x < 2 || x > 1000 || (x - Math.floor(x)) > 0 ) {return 'Данные не верны';}
+  if(typeof x !== 'number' || isNaN(x) || x < 2 || x > 1000 || (x - Math.floor(x)) > 0 ) {return 'Данные неверны';}
   var result = 'Число ' + x + ' - простое число';
   for(i=x-1; i>1; i--) {
     if(!(x % i)) {result = 'Число ' + x + ' - составное число'; break;}
