ea7d1666fcd611a2ba57216280eb9abe76f87129
Anton Kanopkin <39925338+kanstudio@users.noreply.github.com>
2018-10-03 16:19:48 +0200

    Update task-01.js

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var detectType = function(x) {
+function detectType(x) {
   if(typeof x === 'number' || typeof x === 'string') {return typeof x;}
   else {return 'undefined';}
 }
 
-module.exports = <detectType>;
+module.exports = detectType;
