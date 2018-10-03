73c15a7f4937a7730a6b8ce32581c603904e43a5
dchosh <dmitrylbov@gmail.com>
2018-10-03 23:53:16 +0300

    add fourth task

---
+++ src/ex2_js-basics-part2/task-04.js
@@ -0,0 +1,18 @@
+
+function isSame(someArray){
+
+    var result = true;
+
+    var i = 1;
+    
+    while(result && i<someArray.length){
+        if (someArray[i] != someArray[0]){
+            result = false;
+        }
+        i++;
+    }
+
+    return result;
+}
+
+module.exports = isSame;
