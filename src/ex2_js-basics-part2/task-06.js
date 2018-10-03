813de39e57feb9386ad4b7e1ed77c0109a17f708
Anton Kanopkin <39925338+kanstudio@users.noreply.github.com>
2018-10-03 17:12:45 +0200

    Update task-06.js

--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -1,9 +1,10 @@
 'use strict';
 
 function isSimpleNumber(x) {
+  var i;
   if(typeof x !== 'number' || isNaN(x) || x < 2 || x > 1000 || (x - Math.floor(x)) > 0 ) {return 'Данные не верны';}
   var result = 'Число ' + x + ' - простое число';
-  for(var i=x-1; i>1; i--) {
+  for(i=x-1; i>1; i--) {
     if(!(x % i)) {result = 'Число ' + x + ' - составное число'; break;}
   }
   return result;
