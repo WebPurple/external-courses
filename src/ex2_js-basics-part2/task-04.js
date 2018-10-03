1203d115935d429432717f187a42f0cfeb35aaf8
Anton Kanopkin <39925338+kanstudio@users.noreply.github.com>
2018-10-03 17:12:07 +0200

    Update task-04.js

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -1,14 +1,15 @@
 'use strict';
 
 function isAllElementsEqual(arr) {
+  var i;
   switch(arr.length) {
     case 0: return false;
     case 1: return true;
     default:
-      var i = 0;
+      i = 0;
       while(i<arr.length && arr[i++] === arr[i]);
       if(i === arr.length) {return true;}
-      else {return false;}
+      return false;
   }
 }
 
