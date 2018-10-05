b31d3cc9afd3175ebf301fe7f245ea9ca387e078
epam <epam@local.rsu.edu.ru>
2018-10-05 20:45:59 +0300

    task01

---
+++ src/ex2_js-basics-part2/task01.js
@@ -0,0 +1,8 @@
+var f = function(a) {
+    if (typeof (a) == 'string') {
+    console.log('string')
+    } else if (typeof (a) == 'number') {
+        console.log('number')
+    } else console.log('undefined') 
+}
+
