524de8f42cd37e8023b2c9523c1a17735d81c892
epam <epam@local.rsu.edu.ru>
2018-10-05 20:40:18 +0300

    Second

--- src/ex2_js-basics-part2/task03.js
+++ src/ex2_js-basics-part2/task03.js
@@ -1,6 +1,11 @@
 var f = function(arr) {
+    var out = [0, 0, 0];
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 0) {++odd}
-        else if ()
+        if (arr[i] === 0) {++out[2]}
+        else if (arr[i] % 2 === 0) {++out[0]}
+        else if (arr[i] %2 !== 0) {++out[1]}
     }
+    console.log(out);
 }
+
+f([1,2,3,4,0]);
