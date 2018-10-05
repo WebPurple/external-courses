f5cc1dcb3ac8ebf24aa6b4b370e04dae0686c66d
Zhora1337 <balakirev.zhora@yandex.ru>
2018-10-05 20:20:04 +0300

    firstLesson

---
+++ src/ex2_js-basics-part2/task01.js
@@ -0,0 +1,9 @@
+var f1 = function (a){
+	console.log(typeof a);
+}
+
+f1(3);
+f1("Hello!");
+f1(true);
+f1([1,2,3,4,]);
+f1(null);
---
+++ src/ex2_js-basics-part2/task02.js
@@ -0,0 +1,8 @@
+var f2 = function (numbers){
+	for (var i = 0; i<numbers.length;i++)
+		console.log(numbers[i]);
+	console.log("Numbers =",i);
+}
+
+
+f2([3,4,5,6,7,8,100]);
