a61b8b6bed97e2418e1839202e0bcb7dd123ef66
evelienfoubert <evelienfoubert@mail.ru>
2018-10-05 21:14:23 +0300

    second

---
+++ src/ex2_js-basics-part2/task02.js
@@ -0,0 +1,7 @@
+// var f = showElements(myArray) {
+//     for (var i = 0; i < myArr.length; i++){
+//         console.log(myArray[i]);
+//     }
+// }
+// console.log(myArray.length);
+// myArray = [2, 4, 6];
