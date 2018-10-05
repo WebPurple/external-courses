bbad744256c5257109fdb9dca53e12c0468ab1d1
epam <epam@local.rsu.edu.ru>
2018-10-05 20:17:50 +0300

    'task01'

---
+++ src/ex2_js-basics-part2/task02.js
@@ -0,0 +1,7 @@
+var myFunc = function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+    console.log('Total elements of array - '+arr.length)
+}
+myFunc([1,2,34,5,6,78,68,9,5])
