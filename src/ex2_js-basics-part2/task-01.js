6ddda2493d9644570a224ff004a8eddae37baecd
kutur15 <kutur15@hotmail.com>
2018-10-07 00:06:16 +0300

    task02 part01 attempt03

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,12 +1,13 @@
 function MyFunc1 (n){
 	var type1 = typeof n
 if (type1 === "number")
-    {alert ("number");
+    {return "number";
     }
      else if (type1 === "string")
-	 {alert ("string");
+	 {return "string";
 	 	 }	
 		else {
-		alert ("undefined");
+		return "undefined";
 		}
-		}
+        }
+        module.exports = MyFunc1
