788e3ea282c5f7af49150500ff9afcbffdb4ecc1
kutur15 <kutur15@hotmail.com>
2018-10-06 23:16:28 +0300

    task02 part01 attempt02

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,3 +1,12 @@
-var func = function name(params) {
-    
-}
+function MyFunc1 (n){
+	var type1 = typeof n
+if (type1 === "number")
+    {alert ("number");
+    }
+     else if (type1 === "string")
+	 {alert ("string");
+	 	 }	
+		else {
+		alert ("undefined");
+		}
+		}
