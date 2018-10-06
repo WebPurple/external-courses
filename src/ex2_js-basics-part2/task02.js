4dd7824d95d1c47d21c1a564e47c34887f481399
evelienfoubert <evelienfoubert@mail.ru>
2018-10-06 22:14:53 +0300

    added task02

--- src/ex2_js-basics-part2/task02.js
+++ src/ex2_js-basics-part2/task02.js
@@ -1,7 +1,12 @@
-// var f = showElements(myArray) {
-//     for (var i = 0; i < myArr.length; i++){
-//         console.log(myArray[i]);
-//     }
-// }
-// console.log(myArray.length);
-// myArray = [2, 4, 6];
+// ## Задание 2
+
+// Написать функцию, которая будет пробегать по массиву и выводить в консоль каждый элемент массива и общее число элементов.
+var array = [2,4,6,8];
+function showElements(array){
+    for(var i = 0; i < array.length; i++){
+        console.log(array[i]);
+    } console.log(array.length);
+}
+showElements(array);
+showElements([1,10,100,1000,10000]);
+showElements(["a","b","c"]);
