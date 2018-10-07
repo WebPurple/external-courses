c1813c0faae65d2f4ec9b8c35856b14a19d1cff0
kutur15 <kutur15@hotmail.com>
2018-10-07 16:18:46 +0300

    task02 part02

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,5 +1,10 @@
-var NumberArr = [1, 2, 3, 4, 5, 6, 7];
-
-for (var i = 0; i < NumberArr.length; i++) {
-    console.log( NumberArr [i] + " Число элементов: " + NumberArr.length);
-}
+function MyFuncSprint (){ 
+    var NumberArr = [1, 2, 3];
+    var n = 0
+    for (var i = 0; i < NumberArr.length; i++) {
+        if (i % 2 == 0) 
+        var n = n + 1
+        console.log( NumberArr [i] + " Число элементов: " + NumberArr.length);
+    }
+    }
+    module.exports = MyFuncSprint;
