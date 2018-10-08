2fc49f942612d07b01e9054d219541f4666e1c92
unknown <alena.belikova.epam@gmail.com>
2018-10-08 23:42:55 +0300

    task03

--- src/ex2_js-basics-part2/task03.js
+++ src/ex2_js-basics-part2/task03.js
@@ -1,5 +1,11 @@
-for (var i=0; i<5; i++) {
-    if (i % 2 == 0 || i % 2 !== 0 || i === 0) {
-        console.log (i);
-    }
+var f = function (num) {
+    var even = 0;
+    var odd = 0;
+    var zeros = 0;
+    for ( var i = 0; i < num.length; i++)
+    if (num [i] <= 0) zeros ++;
+    else if (num [i] % 2) even++;
+    else odd++;
+    console.log ("четных: ", even,"; нечетных: ", odd, "; нулей: ", zeros,";");
 }
+f ([0,2,3,5,6])
