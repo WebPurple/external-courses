ddec7342d65e0ae42878ff93ee8eeaff8ab73675
unknown <alena.belikova.epam@gmail.com>
2018-10-09 00:07:13 +0300

    task04

--- src/ex2_js-basics-part2/task04.js
+++ src/ex2_js-basics-part2/task04.js
@@ -1,10 +1,10 @@
 var f = function (arr) {
     var t = true;
-    for (var i=i;  i < arr.length; i++) {
+    for (var i=1;  i < arr.length; i++) {
         if (t) {
             if (arr[i-1] !==  arr[i]) t = false;
         }
     }
 console.log (t); 
 }
-f ([10,15,6,6,8,6])
+f ([10,10])
