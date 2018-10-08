504e0afcf70f9e646ccc34c4438b89c7b5c04a7a
Pavel_Uvarov <pavel_uvarov@epam.com>
2018-10-08 12:37:44 +0300

    add task 2 for js-context

--- __tests__/ex14_js-context-task-1.test.js
+++ __tests__/ex14_js-context-task-1.test.js
@@ -1,7 +1,9 @@
+
+
 const task = require('~utils').createTask('ex14_js-context');
 
-describe('Ex14. JS Context', () => {
-    task(code => {
+describe('Ex14. JS Context. Task 1', () => {
+    task('01', code => {
         beforeEach(() => {
             code.reset && code.reset();
         });
