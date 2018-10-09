dadd9e1d055fa66c6190f1b63ecbca80ce47ab2a
Georgy Pecheritsa <39403411+Zhora1337@users.noreply.github.com>
2018-10-09 21:18:14 +0300

    Revert "firstLesson"

--- src/ex2_js-basics-part2/task01.js
+++
@@ -1,9 +0,0 @@
-var f1 = function (a){
-	console.log(typeof a);
-}
-
-f1(3);
-f1("Hello!");
-f1(true);
-f1([1,2,3,4,]);
-f1(null);
--- src/ex2_js-basics-part2/task05.js
+++
@@ -1,9 +0,0 @@
-var f5 = function(numbers) {
-	var max = numbers[0];
-	for (var i = 0; i<numbers.length; i++){
-		if (max<numbers[i]) max = numbers[i];
-	}
-	console.log(max);
-}
-
-f5([12,-3,56,14]);
