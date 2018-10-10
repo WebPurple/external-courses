14750aed0476b276eebe8cc1fe0feec8f9f554f6
MaryMarr <40734809+MaryMarr@users.noreply.github.com>
2018-10-10 12:15:01 +0300

    Update task-05.js

--- src/ex3_js-objects-part1/task-05.js
+++ src/ex3_js-objects-part1/task-05.js
@@ -3,7 +3,9 @@ function makeClone(object) {
     var clone = {};
     var key;
     for (key in object) {
+        if (obj.hasOwnProperty(key)) {
         clone[key] = object[key];
+        }
     }
     return clone;
 };
