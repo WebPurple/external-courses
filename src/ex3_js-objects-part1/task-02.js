12b34deaeebd047b5af05aefde33a31e67a3e24f
MaryMarr <40734809+MaryMarr@users.noreply.github.com>
2018-10-10 12:14:02 +0300

    Update task-02.js

--- src/ex3_js-objects-part1/task-02.js
+++ src/ex3_js-objects-part1/task-02.js
@@ -4,7 +4,7 @@ function logAllKeyValuePairs(obj) {
     for (key in obj) {
         if (obj.hasOwnProperty(key)) {
             console.log('Key: ' + key + 'property ' + obj[key]);
-        };
-    };
-};
+        }
+    }
+}
 module.exports = logAllKeyValuePairs; 
