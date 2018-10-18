1446ed73fc54d9e5a095c624acfd4b28fa463ae0
Анастасия Рубцова <Anastasia@MacBook-Pro-Anastasia.local>
2018-10-19 00:34:28 +0300

    task01

--- src/ex4_js-objects-part2/task01.js
+++ src/ex4_js-objects-part2/task01.js
@@ -1,11 +1,3 @@
-var obj = {
-    a:1,
-    b:2,
-    c:3
-};
-var objNew = Object.create(obj);
-objNew.d=4;
-
 function findProperty(objNew, prop) {
         if (objNew.__proto__[prop] !== undefined) {
         return true
@@ -13,8 +5,3 @@ function findProperty(objNew, prop) {
             return false
         }
 }
-
-var test1=findProperty(objNew, 'b');
-console.log(test1);
-var test2=findProperty(objNew, 'd');
-console.log(test2);
