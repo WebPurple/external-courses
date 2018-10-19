fcef99857765b7831f8b82e52ffb632ddd04a46f
epam <alena.belikova.epam@gmail.com>
2018-10-19 20:26:28 +0300

    task01

--- src/ex4_js-objects-part2/task01.js
+++ src/ex4_js-objects-part2/task01.js
@@ -1,5 +1,11 @@
-var getProperty = function (propName, obj) {
+var getProperty = function (obj, propName) {
 return obj.__proto__[propName];
 }
-Object.create()
-getProperty ()
+
+var obj = {a: 5, b: 6}
+var obj1 = Object.create(obj);
+obj.a=8;
+obj.b=10;
+console.log (getProperty (obj, "a"));
+console.log (obj.a);
+console.log (getProperty(obj ,"b"));
