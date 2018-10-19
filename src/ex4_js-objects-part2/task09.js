254a820533f426cc2bee19c4655798d66d3de649
Анастасия Рубцова <Anastasia@MBP-Anastasia.el.rspu>
2018-10-19 20:48:27 +0300

    task09

--- src/ex4_js-objects-part2/task09.js
+++ src/ex4_js-objects-part2/task09.js
@@ -1,15 +1,18 @@
-var str = 'I want to sleep';
-var num = 3;
-var str1 = 'very';
-
 function newString(str, str1, num) {
     var strNew = str.split(' ');
-    var strNew1;
-    for (var i = 0; i <= num; i++) {
-        strNew1 += strNew[i]
+    var strNew1 = '';
+    for (var i = 0; i <= num-1; i++) {
+        strNew1 += strNew[i] + ' ';
     }
-    console.log(strNew1)
-
+    strNew1 += str1 + ' ';
+    for (var j = num; j < strNew.length; j++) {
+        strNew1 += strNew[j] + ' ';
+    }
+    return strNew1
 }
 
-newString(str, str1, num)
+function newString1(str, str1, num) {
+    var strNew = str.split(' ');
+    strNew.splice(num, 0, str1);
+    return strNew.join(' ')
+}
