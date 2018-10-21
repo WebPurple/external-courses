024abe0cf493acff3eed2c5e722a9c44979e0e8a
mrcelviano <nikita.sergienko.97@gmail.com>
2018-10-21 17:29:46 +0300

    task_is_ready

--- src/ex3_js-objects-part1/task_06.js
+++ src/ex3_js-objects-part1/task_06.js
@@ -1,14 +1,27 @@
 "use strict"; 
 
-function objectProcessing(object){
-    var clone = {};
+function clone(obj) {
+
+    var temp = null;
     var key = null;
 
-    for (key in object ){
-        clone[key] = object[key];
+    if (obj === null || typeof (obj) !== 'object' || 'isActiveClone' in obj)
+        return obj;
+
+    if (obj instanceof Date)
+        temp = new obj.constructor();
+    else
+        temp = obj.constructor();
+
+    for (key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            obj['isActiveClone'] = null;
+            temp[key] = clone(obj[key]);
+            delete obj['isActiveClone'];
+        }
     }
 
-    return clone;
+    return temp;
 }
 
-module.exports = objectProcessing;
+module.exports = clone;
