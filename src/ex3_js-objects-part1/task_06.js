d061691201066603d01c59f0517a040cf752fb49
mrcelviano <nikita.sergienko.97@gmail.com>
2018-10-21 18:36:30 +0300

    task_is_ready

--- src/ex3_js-objects-part1/task_06.js
+++ src/ex3_js-objects-part1/task_06.js
@@ -7,7 +7,7 @@ function cloneObject(object) {
 
         for (property in object) { 
             if (object.hasOwnProperty(property)) { 
-                if ("object" === typeof(object[property])) {
+                if (typeof object[property] === "object") {
                     clone[property] = makeClone(object[property]); 
                  } else {
                     clone[property] = object[property]; 
