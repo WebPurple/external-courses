b9f71d11f8874f92c4848aedcecb2468547b825d
esamorukova <lenass97@mail.ru>
2018-10-23 19:11:26 +0300

    task

---
+++ src/ex4_js-objects-part2/task_14.js
@@ -0,0 +1,9 @@
+var str0 = "Hello world";
+var str00 = 'elo'
+
+function ranNum(min, max) {
+     var result = Math.random()*(max-min) + min;
+     return Math.floor(result)
+    
+}
+console.log(ranNum(2,9))
