5cabca3ef4420946bf64168204f47483979b091f
dchosh <dmitrylbov@gmail.com>
2018-12-20 00:56:12 +0300

    fix scandom

--- src/ex12/scanDOM.js
+++ src/ex12/scanDOM.js
@@ -34,52 +34,52 @@ function scanDOM(){
 }
 
 
-var tags = {};
-var classes = {};
 
+var nodelist = {'textnode': 0};
+var classlist = {};
 
-function scanDOMRecursive(elem){
-    
-    let element = elem;
-    
-    if (elem === undefined){
-        element = document.getElementsByTagName('html')[0];
-        
-    }
-    
-    if (tags[element.tagName] !== undefined){
-        tags[element.tagName] += 1;
-    } else {
-        tags[element.tagName] = 1;
-    }
-    
-    element.classList.forEach (tempClass => {
-        if (classes[tempClass] !== undefined){
-            classes[tempClass] += 1;
-        } else {
-            classes[tempClass] = 1;
+function scanDOMRecursive(node){
+
+    //элемент
+    if (node.nodeType === 1){ 
+
+        if (nodelist[node.tagName] === undefined) {
+            nodelist[node.tagName] = 0;
         }
-    });
-    let children = element.children;
-    
-    for (let i = 0; i < children.length; i++){
-        scanDOMRecursive(children[i]);
-    } 
-    
-    if (element.tagName === 'HTML'){
-        console.log('Тэги: ')
-        Object.keys(tags).forEach( key =>{
-            console.log('Тэгов ' + key + ": " + tags[key]);
+
+        nodelist[node.tagName] += 1;
+        
+        node.classList.forEach(element => {
+            if (classlist[element] === undefined) {
+                classlist[element] = 0;
+            }
+
+            classlist[element] += 1;
         });
 
-        console.log('Классы: ')
-        Object.keys(classes).forEach( key =>{
-            console.log('Элементов класса  ' + key + ": " + classes[key]);
+        node.childNodes.forEach(element => {
+            scanDOMRecursive(element)
+        });
+    //текстовая нода
+    } else if (node.nodeType === 3){
+
+        nodelist.textnode += 1;
+
+    }
+    
+    if (node.tagName === "HTML") {
+        console.log("Элементы_______________");
+        Object.keys(nodelist).forEach(element => {
+            console.log("Элементов " + element + " - " + nodelist[element]);
+        });
+
+        console.log("Классы_________________");
+        Object.keys(classlist).forEach(element => {
+            console.log("Элементов класса " + element + " - " + classlist[element]);
         });
     }
 }
 
 
-
 // scanDOM();
-scanDOMRecursive();
+scanDOMRecursive(document.getElementsByTagName("HTML")[0]);
