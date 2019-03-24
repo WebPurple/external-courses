8dd9335f682a1ba899559ff4dd30f343c5d109ce
elenap1991 <pirogovael91@gmail.com>
2019-03-24 18:06:23 +0300

    deleted file string utils

--- src/ex4_js-objects-part2/string_utils.js
+++
@@ -1,7 +0,0 @@
-var stringUtils = {}
-stringUtils.toUpperCaseSymb = function (str) {
-    var newstr = str.toLowerCase();
-    var firstSymb = newstr.charAt(0).toUpperCase();
-    return firstSymb + newstr.slice(1);
-};
-module.exports = stringUtils
