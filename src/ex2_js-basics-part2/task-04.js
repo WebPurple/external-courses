f6f22e04a1df65033f57e1e6b8b7e5183167b850
Georgy <chirkovgeorgii@mail.ru>
2019-09-26 19:40:46 +0300

    fix task4

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -1,17 +1,12 @@
 function checkSameArrays(argument) {
     let count =1;
     let isSame=true;
-    while ((count <=argument.length)&&(isSame===true)) {
+    while ((count <argument.length)&&(isSame===true)) {
         if (argument[0]!==argument[count]) {
             isSame=false;
         }
         count++;
     }
-    if (isSame) {
-        return(console.log("Arrays are same"))
-    }
-    else {// eslint-disable-line no-else-return
-        return(console.log("Arrays are different"))
-    }
+    return(isSame)
 }
 module.exports = checkSameArrays;
