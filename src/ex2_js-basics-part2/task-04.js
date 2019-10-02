a1795b89c4c5b11b4de633b6244c15da5495734c
hordesnake <dlyakovl@gmail.com>
2019-10-02 03:21:08 +0300

    added task-06 + use strict

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -1,10 +1,12 @@
+'use strict';
+
 function sameElements(arr) {
-    let isSameElements = true;
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[0] !== arr[i]) {
-            isSameElements = false
-        };
-    }
-    return isSameElements;
+  let isSameElements = true;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[0] !== arr[i]) {
+      isSameElements = false
+    };
+  }
+  return isSameElements;
 }
 module.exports = sameElements;
