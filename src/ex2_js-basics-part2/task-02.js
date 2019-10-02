d982f2647be52e61a3c54ce27c4c1a64de6b40ac
hordesnake <dlyakovl@gmail.com>
2019-10-02 03:12:31 +0300

    reffactoring all task 2

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,8 +1,7 @@
-function outputArr(arr){								
-	for(let i = 0; i < arr.length; i++)	{
-		console.log(i + 1 + ' элемент массива: ' + arr[i]);
-	}
-	console.log('Длина массива: ' + arr.length);		
-	return arr;
+function outputArr(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(i + 1 + ' элемент массива: ' + arr[i]);
+    }
+    console.log('Длина массива: ' + arr.length);
 }
 module.exports = outputArr;
