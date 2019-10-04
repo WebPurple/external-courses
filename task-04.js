884561430cfd900c07b2d2da8b1d3eeb91e9969e
SergeyEroshkin <55455413+SergeyEroshkin@users.noreply.github.com>
2019-10-04 03:16:04 +0300

    Delete task-04.js
    It is mistake

--- task-04.js
+++
@@ -1,7 +0,0 @@
-function equalityArrayElements(Arr) {
-    let count = 1, elementsEqual = true;
-    while ((count < Arr.length) && (elementsEqual === true)) {
-        if (Arr[0] !== Arr[count]) {
-            elementsEqual = false;
-        }
-      
