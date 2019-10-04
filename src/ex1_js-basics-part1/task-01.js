467c9a8146cffddd88038d9dafcf26793fce8cd7
Commandos <pavelvlas_v@icloud.com>
2019-10-04 13:34:40 +0300

    Сlean

--- src/ex1_js-basics-part1/task-01.js
+++
@@ -1,8 +0,0 @@
-function getExpression (type) {
-	if (typeof type ==="string") {
-		return "string";
-	} else if (typeof type ==="number") {
-		return "number"; 
-	}	return "undefined";
-}
-module.exports = getExpression;
--- src/ex1_js-basics-part1/task-02.js
+++
@@ -1,8 +0,0 @@
-function checkCalcOfMass(){
-	for (num of _Mass){
-       console.log(num);
-	}
-	let _length = _Mass.length;
-    console.log (`Number of array elements = ${_length}`);
-}
-module.exports = checkCalcOfMass;
