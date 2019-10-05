aaae356c872b63b1e2ff8e491711ae8c23660b7c
Georgy <chirkovgeorgii@mail.ru>
2019-10-05 22:36:51 +0300

    Delete unnecessary comments

--- src/ex4_js-objects-part2/task-01.js
+++ src/ex4_js-objects-part2/task-01.js
@@ -1,5 +1,4 @@
 function findPropInProto (prop, obj){//eslint-disable-line consistent-return
-    //const obj = Object.create({});
     for (let key in Object.getPrototypeOf(obj)) {
         if (prop===key){
             return obj[prop]
--- src/ex4_js-objects-part2/task-06.js
+++ src/ex4_js-objects-part2/task-06.js
@@ -1,5 +1,4 @@
 function upFirstCharAll (str){
     return str.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ')
-    //return str;
 }
 module.exports = upFirstCharAll;
