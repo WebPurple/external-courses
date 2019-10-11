7b6d4adb333fc44655b1e9d9b24932b3c71a8c9e
Georgy <chirkovgeorgii@mail.ru>
2019-10-11 20:43:43 +0300

    Delete all unnececary files

--- src/ex2_js-basics-part2/task-02.js
+++
@@ -1,9 +0,0 @@
-function displayArray(arr) {
-    let count=0, numberOfElements=1;
-    while (count <arr.length){
-        console.log(arr[count]);
-        count++;
-    }
-    return console.log("Number of elements", arr.length);
-}
-module.exports = displayArray;
--- src/ex2_js-basics-part2/task-05.js
+++
@@ -1,9 +0,0 @@
-function maxArrayElement(arr) {
-    let maxElement=0, count=0;
-    while (count<arr.length) {
-        if (arr[count] > maxElement) maxElement = arr[count];
-    count++;
-    }
-    return maxElement;
-}
-module.exports = maxArrayElement;
