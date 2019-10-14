87d07ea86b7edf4a948a5c29f22d49543cdf2122
SergeyEroshkin <55455413+SergeyEroshkin@users.noreply.github.com>
2019-10-14 05:59:32 +0300

    Update task-04.js

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -1,6 +1,6 @@
 function equalityArrayElements(arr) {
     for (var b = 0; b < arr.length; b++)
-      if (arr[b] !== arr[0]) return 'false';
-    return 'true';
+      if (arr[b] !== arr[0]) return false;
+    return true;
 }
 module.exports = equalityArrayElements; 
