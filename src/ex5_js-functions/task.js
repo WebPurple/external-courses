a909210c43ddb18776a0b6979456fc54d921a3b2
Georgy <chirkovgeorgii@mail.ru>
2019-10-17 18:09:06 +0300

    Delete nameed function expression

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,25 +1,25 @@
 (function Calculator(){
   let res = 0;
-  const add = function add(x=0){
+  function add(x=0){
     res += x;
     return add;
   }
-  const getResult = function result() {
+  function result() {
       return res
   }
-  const subtract = function subtract(x=0){
+  function subtract(x=0){
       res -= x;
       return subtract;
   }
-  const divide = function divide(x=1){
+  function divide(x=1){
       res /= x;
       return divide;
   }
-  const multiply = function multiply(x=1){
+  function multiply(x=1){
       res *= x;
       return multiply
   }
-  const reset = function reset(){
+  function reset(){
       res = 0;
       return res;
   }
