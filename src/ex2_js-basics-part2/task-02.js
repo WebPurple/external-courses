29b6273d241f04ba5394192f5b55dab7b02a1edd
irkarpekina@mail.ru <irkarpekina@mail.ru>
2020-03-03 11:39:32 +0300

    Revert "init: add task-02 solution"
    This reverts commit 11e323292c5351591918dcc28c23930b3ff7907e.

--- src/ex2_js-basics-part2/task-02.js
+++
@@ -1,8 +0,0 @@
-function getElementArr(arr){
-	for ( let i=0; i<arr.length; i++){
-  		console.log(arr [i])
-  	}
-  	console.log(arr.length);
-  	return undefined;
-}
-module.exports = getElementArr;
