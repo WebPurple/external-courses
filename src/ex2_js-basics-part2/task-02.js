3dead57b84f5d1008c768daebf829bed8493c977
irkarpekina@mail.ru <irkarpekina@mail.ru>
2020-03-03 12:17:31 +0300

    init: add task-02 solution

---
+++ src/ex2_js-basics-part2/task-02.js
@@ -0,0 +1,8 @@
+function getElementArr(arr){
+	for ( let i=0; i<arr.length; i++){
+  		console.log(arr [i])
+  	}
+  	console.log(arr.length);
+  	return undefined;
+}
+module.exports = getElementArr;
