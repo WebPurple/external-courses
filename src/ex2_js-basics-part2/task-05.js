0091c9994859763a972535eea0292677517e1004
ryze9480 <ryze9480@yandex.ru>
2020-03-09 11:32:25 +0300

    add 5th task

--- src/ex2_js-basics-part2/task-05.js
+++ src/ex2_js-basics-part2/task-05.js
@@ -1,13 +1,11 @@
-
-
 function getMaxElement(array){
-let max=0;
+  let max=0;
   for(i=0;i<array.length;i++){
     if(max<array[i]){
       max=array[i]
     }
-  
   }
   return(max)
 }
+
 module.exports=getMaxElement
