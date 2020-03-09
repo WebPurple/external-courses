8edbcc263bd2c3022acff11583b0de3ff18a9a47
ryze9480 <ryze9480@yandex.ru>
2020-03-09 08:30:57 +0300

    task-05.js

---
+++ src/ex2_js-basics-part2/task-05.js
@@ -0,0 +1,13 @@
+
+
+function getMaxElement(array){
+let max=0;
+  for(i=0;i<array.length;i++){
+    if(max<array[i]){
+      max=array[i]
+    }
+  
+  }
+  return(max)
+}
+module.exports=getMaxElement
