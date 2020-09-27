e56b841fdfb241ab4d2c0360c2fc90e05ea15e7e
Marcsc777 <markryazan@mail.ru>
2020-09-27 18:07:16 +0300

    add index

--- src/ex2_js-basics-part2/task-02.js
+++
@@ -1,8 +0,0 @@
-module.exports = function countArray (myArray) {
-    let counter = 0;
-    for (let elem of myArray) {
-        console.log(myArray[counter]);
-        counter++;
-    }
-    return console.log(myArray.length);
-}
--- src/ex2_js-basics-part2/task-04.js
+++
@@ -1,3 +0,0 @@
-module.exports = function equallyArray (myArray) {
-    return new Set(myArray).size === 1;
-}
