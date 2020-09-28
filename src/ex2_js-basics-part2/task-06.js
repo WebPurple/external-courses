57b3771defa58444e6932047bdce2b6c22b14669
Marcsc777 <markryazan@mail.ru>
2020-09-28 07:28:22 +0300

    ex2_js-basics-part2 - ready

--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -3,9 +3,9 @@ module.exports = function numberUnder1000SimpleOrComplex (MyNumber){
 	if (MyNumber === 0) return '0 не натуральное число';
 	if (MyNumber === 1) return '1 не составное и не простое число';
 	for(let i = 2; i < MyNumber; i++){
-			if (MyNumber % i === 0){
-				return 'Число ' + MyNumber + ' - составное число'
-			}
+		if (MyNumber % i === 0){
+			return 'Число ' + MyNumber + ' - составное число'
 		}
+	}
 	return 'Число ' + MyNumber + ' - простое число';	
 }
