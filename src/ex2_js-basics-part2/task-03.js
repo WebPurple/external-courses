ab84826558b82ff92f3285742ad051ddc8547eac
Ardent24 <wrb.re-cords@yandex.ru>
2020-10-07 22:01:11 +0300

    test

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -1,15 +1,15 @@
 function checkEvenArray(array) {
-  let [evenNum, oddNum, zero] = [0, 0, 0];
+  let [evenNum, oddNum, zeroNum] = [0, 0, 0];
 
   for (let elem of array) {
     if (!(typeof elem === "string")) {
-      if (elem === 0) zero++;
+      if (elem === 0) zeroNum++;
       if (elem % 2 === 0 && elem !== 0 && elem !== null) evenNum++;
       if (elem % 2 === 1) oddNum++;
     }
   }
 
-  return [evenNum, oddNum, zero];
+  return [evenNum, oddNum, zeroNum];
 }
 
 module.exports = checkEvenArray;
