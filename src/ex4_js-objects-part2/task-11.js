2dd6f8127dd27755d4a674550578f9c17ec25196
Ardent24 <wrb.re-cords@yandex.ru>
2020-10-12 22:50:03 +0300

    ex4-branch: work on bugs

--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -2,10 +2,10 @@ function countLetters(str) {
   const obj = {};
 
   for (let i = 0; i < str.length; i++) {
-    if (obj[str[i]] === undefined) {
-      obj[str[i]] = 0;
-    } else {
+    if (obj[str[i]]) {
       obj[str[i]] = obj[str[i]] + 1;
+    } else {
+      obj[str[i]] = 0;
     }
   }
 
