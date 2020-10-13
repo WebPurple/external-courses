056b7e8b0c7dfc5b50f5deca78576cabd5a6b73c
Alexandr Pikulin <apikulin437@gmail.com>
2020-10-14 01:39:59 +0300

    test git

--- src/ex4_js-objects-part2/task-06.js
+++ src/ex4_js-objects-part2/task-06.js
@@ -3,7 +3,7 @@ function wordToUpperCase(str) {
   const arrNewStr = [];
 
   for (let i = 0; i < arrStr.length; i++) {
-    arrNewStr[i] = arrStr[i][0].toUpperCase() + arrStr[i].slice(1);
+     arrNewStr[i] = arrStr[i][0].toUpperCase() + arrStr[i].slice(1);
   }
 
   return arrNewStr.join(" ");
