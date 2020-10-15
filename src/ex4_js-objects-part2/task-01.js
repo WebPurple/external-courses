0dc03fb9af9da5d3f7420eb5106f8f28c44c1d15
Alexandr Pikulin <apikulin437@gmail.com>
2020-10-15 21:35:59 +0300

    task01

--- src/ex4_js-objects-part2/task-01.js
+++ src/ex4_js-objects-part2/task-01.js
@@ -1,9 +1,9 @@
 function returnValueObj(prop, obj) {
   if (Object.getPrototypeOf(obj).hasOwnProperty(prop)) {
     return Object.getPrototypeOf(obj)[prop];
-  } else {
-    return undefined;
   }
+  
+  return undefined;
 }
 
 module.exports = returnValueObj;
