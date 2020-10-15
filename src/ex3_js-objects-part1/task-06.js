7ab08b640e37a267fe90b96e8da54498f58b6b45
Alexandr Pikulin <apikulin437@gmail.com>
2020-10-15 19:38:04 +0300

    add task 6

---
+++ src/ex3_js-objects-part1/task-06.js
@@ -0,0 +1,23 @@
+function cloneObj(obj) {
+  const objCopy = {};
+
+  for (let key in obj) {
+    if (Array.isArray(obj[key])) {
+      objCopy[key] = obj[key].map((val) => {
+        if (typeof val == "object") {
+          return cloneObj(val);
+        }
+        
+        return val;
+      });
+    } else if (typeof obj[key] === "object") {
+      objCopy[key] = cloneObj(obj[key]);
+    } else {
+      objCopy[key] = obj[key];
+    }
+  }
+
+  return objCopy;
+}
+
+module.exports = cloneObj;
