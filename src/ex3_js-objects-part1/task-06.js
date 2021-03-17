75265d6e83b1037d8ad7fd92a77bb52e7de938b5
Skunderr <nikolairgrtu@gmail.com>
2021-03-17 21:40:03 +0400

    fixed task-06.js

--- src/ex3_js-objects-part1/task-06.js
+++ src/ex3_js-objects-part1/task-06.js
@@ -2,26 +2,29 @@ function getDeepClone(clonedObj) {
     let clone;
 
     if (null === clonedObj || clonedObj !== "object") {
-        return clonedObj;
+        return Object.assign({}, clonedObj);
     }
 
     if (Array.isArray(clonedObj)) {
         clone = [];
 
-        clonedObj.forEach(elem => clone.push(getDeepClone(elem)));
+        clonedObj.forEach(elem => {
+            clone.push(getDeepClone(elem));
+        });
         
         return clone;
     }
 
-    clone = {};
-
     for (let elem in clonedObj) {
+
         if (clonedObj.hasOwnProperty(elem)) {
+            clone = {};
+
             clone[elem] = getDeepClone(elem);
-        }     
-    }
-    
-    return clone;
+        }   
+        
+        return clone;    
+    } 
 }
 
 module.exports = getDeepClone;
