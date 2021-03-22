41eb8e8f225c7c02bc97ebfb748ac2dbfc3ddbf1
Skunderr <nikolairgrtu@gmail.com>
2021-03-22 22:43:30 +0400

    fixed task-03.js, task-11.js

--- src/ex4_js-objects-part2/task-03.js
+++ src/ex4_js-objects-part2/task-03.js
@@ -1,5 +1,13 @@
-function deleteFirstAndLastObj(str) {
-    return str.trim();
-}
+function deleteFirstAndLastSpace(str) {
+    const splitStr = str.split('')
+  
+    if (splitStr[0] === " " && splitStr[splitStr.length-1] === " ") {
+      splitStr.pop();
+      splitStr.shift();
+    }
+    
+    return splitStr.join("")
+  
+  }
 
-module.exports = deleteFirstAndLastObj;
+module.exports = deleteFirstAndLastSpace;
--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -1,7 +1,7 @@
 function getNumberOfRepetitions(string) {
   const splitString = string.split('');
   
-  const result = splitString.reduce((result, letter) => (result[letter] = (result[letter] || 0) + 1, result), []);
+  const result = splitString.reduce((result, letter) => (result[letter] = (result[letter] || 0) + 1, result), {});
   
   for (let elem in result) {
     console.log(result[elem])
