8d0109c46d7d044f5dfe3f123757f021d98aa366
No4D <alno4d@gmail.com>
2021-03-29 00:10:22 +0300

    add task.js in ex5

---
+++ src/ex5_js-functions/task.js
@@ -0,0 +1,49 @@
+calculator = {
+    result : 0,
+    add : function(number) {
+        if (typeof(number) == "undefined") {
+            return this.add; 
+        }
+
+        calculator.result = calculator.result + number;
+
+        return this.add;
+    },
+    subtract : function(number) {
+        if (typeof(number) == "undefined") {
+            return this.subtract; 
+        }
+
+        calculator.result = calculator.result - number;
+
+        return this.subtract;
+    },
+    divide : function(number) {
+        if (typeof(number) == "undefined") {
+            return this.divide; 
+        }
+
+        calculator.result = calculator.result / number;
+
+        return this.divide;
+    },
+    multiply : function(number) {
+        if (typeof(number) == "undefined") {
+            return this.multiply; 
+        }
+
+        calculator.result = calculator.result * number;
+
+        return this.multiply;
+    },
+    getResult : function() {
+        return this.result;
+    },
+    reset : function() {
+        this.result = 0;
+        return this.result;
+    }
+};
+
+module.exports = calculator;
+ 
