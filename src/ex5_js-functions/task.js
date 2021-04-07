3cea1e17d3f4e07c32ce09c7e1dd63067d86cb9c
No4D <alno4d@gmail.com>
2021-04-07 03:11:01 +0300

    update task,js in ex5 v2

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,48 +1,44 @@
 function calculator() {
     let result = 0;
-    
-    return {
-        add(number) {
-            if (typeof(number) == "undefined") {
-                return this.add; 
-            }
-            
-            result = result + number; 
-            return this.add;
-        },
-        subtract(number) {
-            if (typeof(number) == "undefined") {
-                return this.subtract; 
-            }
-            
-            result = result - number;
-            
-            return this.subtract;
-        },
-        divide(number) {
-            if (typeof(number) == "undefined") {
-                return this.divide; 
-            }
-            
-            result = result / number;
-            
-            return this.divide;
-        },
-        multiply(number) {
-            if (typeof(number) == "undefined") {
-                return this.multiply; 
-            }
-        
-            result = result * number;
-        
-            return this.multiply;
-        },
-        getResult(){
-            return result;
-        },
-        reset() {
-            result = 0;
+
+    const add = (v) => { 
+        if (v !== undefined) {
+            result = result + v;
         }
+
+        return add;
+    };
+    const subtract = (v) => { 
+        if (v !== undefined) {
+            result = result - v;
+        }
+
+        return subtract;
+    };
+    const divide = (v) => { 
+        if (v !== undefined) {
+            result = result / v;
+        }
+
+        return divide;
+    };
+    const multiply = (v) => { 
+        if (v !== undefined) {
+            result = result * v;
+        }
+
+        return multiply;
+    };
+    const getResult = () => result;
+    const reset = () => (result = 0);
+
+    return {
+       add,
+       subtract, 
+       divide,
+       multiply,
+       getResult,
+       reset,
     }
 };
 
