33f611295ce367321e1e3689acd19b1c07e58399
Skunderr <nikolairgrtu@gmail.com>
2021-04-08 21:56:44 +0300

    fixed task-01

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -2,47 +2,29 @@ function getSliceArr(array, begin, end) {
     const sliceArr = [];
 
     if (arguments.length === 3) {
-        for (let elem of array) {
-            const index = array.indexOf(elem);
-
-            if (index>= begin && index < end) {
-                sliceArr.push(elem);
-            }  
+        if (end > 0) {
+            return sliceArr.concat(array.splice(begin, end-begin));
         }
-
+        
         if (end < 0) {
             array.reverse();
 
-            for (let elem of array) {
-                const index = array.indexOf(elem);
-
-                if (index < Math.abs(begin) && index >= Math.abs(end)) {
-                    sliceArr.push(elem);
+            for (let i = 0; i < array.length; i++) {
+                if (i < Math.abs(begin) && i >= Math.abs(end)) {
+                    sliceArr.push(i);
                 }
             }
 
-            sliceArr.reverse();
+            return sliceArr.reverse();
         }
-
-        return sliceArr;
     }
 
     if (arguments.length === 2) {
-        for (let elem of array) {
-            if (array.indexOf(elem) >= begin) {
-                sliceArr.push(elem);
-            }
-        }
-
-        return sliceArr;
+        return sliceArr.concat(array.splice(begin, array.length));
     }
 
     if (arguments.length === 1) {
-        for (let elem of array) {
-             sliceArr.push(elem);
-        }
-
-        return sliceArr;
+        return  sliceArr.concat(array);
     }
 }
 
