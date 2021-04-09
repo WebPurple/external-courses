18861dc41611070f81b5ce8b9f240ddf13c0f4a9
Skunderr <nikolairgrtu@gmail.com>
2021-04-09 19:15:50 +0300

    fixed task-01

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -1,49 +1,31 @@
 function getSliceArr(array, begin, end) {
     const sliceArr = [];
 
-    if (arguments.length === 3) {
-        for (let elem of array) {
-            const index = array.indexOf(elem);
+    for (let elem of array) {
+        const index = array.indexOf(elem);
 
-            if (index>= begin && index < end) {
-                sliceArr.push(elem);
-            }  
-        }
-
-        if (end < 0) {
+        if (arguments.length === 3 && index >= begin && index < end) {
+            sliceArr.push(elem);
+        } 
+        
+        if (end < 0 && index < Math.abs(begin) && index >= Math.abs(end)) {
             array.reverse();
 
-            for (let elem of array) {
-                const index = array.indexOf(elem);
-
-                if (index < Math.abs(begin) && index >= Math.abs(end)) {
-                    sliceArr.push(elem);
-                }
-            }
+            sliceArr.push(elem);
 
             sliceArr.reverse();
         }
 
-        return sliceArr;
-    }
-
-    if (arguments.length === 2) {
-        for (let elem of array) {
-            if (array.indexOf(elem) >= begin) {
-                sliceArr.push(elem);
-            }
+        if (arguments.length === 2 && index >= begin) {    
+            sliceArr.push(elem);
         }
-
-        return sliceArr;
-    }
-
-    if (arguments.length === 1) {
-        for (let elem of array) {
-             sliceArr.push(elem);
+    
+        if (arguments.length === 1) {
+            sliceArr.push(elem);
         }
-
-        return sliceArr;
     }
+
+    return sliceArr;
 }
 
 module.exports = getSliceArr;
