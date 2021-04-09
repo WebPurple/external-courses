cdf9cdc7b348436f1d1d3b869cb9d11b26ad8c27
Skunderr <nikolairgrtu@gmail.com>
2021-04-09 20:48:36 +0300

    fixed ex6

--- src/ex6_js-array-methods/task-02.js
+++ src/ex6_js-array-methods/task-02.js
@@ -1,6 +1,6 @@
 function analogSome(array, callback, thisArg) {
    for (let i = 0; i < array.length; i++) {
-      if (callback.call(thisArg, array[i], i, array)) {
+      if(callback.call(thisArg, array[i], i, array)) {
          return true;
       }
    } 
