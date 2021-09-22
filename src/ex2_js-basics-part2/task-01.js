da0931a61ad8b76bdad91e4b5e9a0ba6ec573d96
dmitrychernobaev <zeleniy.fingal@gmail.com>
2021-09-22 22:07:23 +0300

    complete task-01

---
+++ src/ex2_js-basics-part2/task-01.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function NumberOrString (taken) {
+
+    if ((typeof taken === 'number') && (!isNaN(taken))) {
+        return('number');
+    } else if (typeof taken === 'string') {
+        return('string');
+    }  else {
+        return(undefined);
+    }
+
+}
+
+module.exports = NumberOrString;
