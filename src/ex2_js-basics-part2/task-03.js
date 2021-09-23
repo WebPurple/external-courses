41857f3cc084f2b279d57e707e45b54a2ea5eb1f
dmitrychernobaev <zeleniy.fingal@gmail.com>
2021-09-23 20:35:31 +0300

    end task3 revisions

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -3,26 +3,25 @@
 
 function KindsArrayNumbers (NeedfullArray) {
 
-    let NumberEven = 0;
-    let NumberOdd = 0;
-    let NumberNull = 0;
+    let NumberArray = [0, 0, 0];
+
     for (let i = 0; i < NeedfullArray.length; i++) {
 
         if (typeof NeedfullArray[i] === 'number') {
             if ((NeedfullArray[i] % 2 === 0) && (NeedfullArray[i] !== 0)) {
-                NumberEven++;
+                NumberArray[0]++;
             } else if (NeedfullArray[i] % 2 !== 0) {
-                NumberOdd++;
+                NumberArray[1]++;
             } else if (NeedfullArray[i] === 0) {
-                NumberNull++;
+                NumberArray[2]++;
             }
         }
     }
 
-    console.log("Number of Even: " + NumberEven);
-    console.log("Number of Odd: " + NumberOdd);
-    console.log("Number of Null: " + NumberNull);
-
+    console.log("Number of Even: " + NumberArray[0]);
+    console.log("Number of Odd: " + NumberArray[1]);
+    console.log("Number of Null: " + NumberArray[2]);
+    console.log(NumberArray);
 }
 
 module.exports = KindsArrayNumbers;
