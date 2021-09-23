8d069aae0d7e4b575b3736123d6249a18af59f27
dmitrychernobaev <zeleniy.fingal@gmail.com>
2021-09-23 18:34:39 +0300

    complete task2

---
+++ src/ex2_js-basics-part2/task-02.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function ArrayAlements (Array) {
+
+    for (let i = 0; i < Array.length; i++) {
+        console.log(Array[i]);
+    }
+    console.log('Array lenght: ', Array.length);
+    return;
+
+}
+
+module.exports = NumberOrString;
