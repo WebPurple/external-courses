f3b50c1f7e4728e82a65d5bc03b3d1ca643017b2
dmitrychernobaev <zeleniy.fingal@gmail.com>
2021-09-24 14:10:02 +0300

    complete task5

---
+++ src/ex2_js-basics-part2/task-05.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function maxElementArray (CurrentArray) {
+
+    let maxElement = CurrentArray[0];
+    for (let i = 0; i < CurrentArray.length; i++){
+        
+        if ((CurrentArray[i] >= 0) && Number.isInteger(CurrentArray[i])) {
+            if (CurrentArray[i] > maxElement) {
+                maxElement = CurrentArray[i];
+            }
+        }  else {
+            console.log('Array have wrong elements');
+            maxElement = 'wrong array';
+            break;
+        }
+    }
+    if (Number.isInteger(maxElement)) {
+        console.log('Max element of array: ' + maxElement);
+    }
+}
+
+module.exports = maxElementArray;
