a25609942865257d54745b8a754abc757ff01fa0
dmitrychernobaev <zeleniy.fingal@gmail.com>
2021-10-15 18:47:36 +0300

    fix practical work 2(++ bug)

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,6 +1,6 @@
 function arrayAlements(array1) {
-  for (let i = 0; i < array1.length; i++) {
-      console.log(array1[i]);
+  for (let i = 0; i < array1.length; i += 1) {
+    console.log(array1[i]);
   }
 
   console.log(`Array lenght: ${array1.length}`);
