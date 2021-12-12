df9d8f6c03e5db381fa3fbd593de38c19db11fd7
dmitrychernobaev <zeleniy.fingal@gmail.com>
2021-12-12 13:54:24 +0300

    ex 14 start work

---
+++ src/ex14_js-context/calculator.js
@@ -0,0 +1,51 @@
+function Calculator() {
+  let currentValue = 0;
+
+  const add = function add(addValue = 0) {
+    currentValue += addValue;
+    return add;
+  };
+  const subtract = function subtract(subtractValue = 0) {
+    currentValue -= subtractValue;
+    return subtract;
+  };
+  const divide = function divide(divideValue = 1) {
+    currentValue /= divideValue;
+    return divide;
+  };
+  const multiply = function multiply(multiplyValue = 1) {
+    currentValue /= multiplyValue;
+    return multiply;
+  };
+  const getResult = function getResult() {
+    console.log(currentValue);
+    return getResult;
+  };
+  const reset = function reset() {
+    currentValue = 0;
+    return reset;
+  };
+  const changeState = function changeState(changesValue = 0) {
+    currentValue = changesValue;
+    return changeState;
+  };
+
+  return {
+    add,
+    subtract,
+    divide,
+    multiply,
+    getResult,
+    reset,
+    changeState,
+  };
+}
+
+const calculator = Calculator();
+
+calculator.add();
+calculator.subtract();
+calculator.divide();
+calculator.multiply();
+
+module.exports = calculator;
