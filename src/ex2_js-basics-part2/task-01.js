07603e5f01d8d6cae8d0e07cbb0323d5ffbd5d2d
Sergey Savinkin <savinoldo@gmail.com>
2022-03-07 14:32:16 +0300

    ex2 task-01 is done

---
+++ src/ex2_js-basics-part2/task-01.js
@@ -0,0 +1,12 @@
+function typeCheck(a) {
+  if (isNaN(a) === true && typeof a !== 'string') {
+    return undefined;
+  } if (typeof a === 'string') {
+    return 'string';
+  } if (typeof a === 'number') {
+    return 'number';
+  }
+  return undefined;
+}
+
+module.exports = typeCheck();
