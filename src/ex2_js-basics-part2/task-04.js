c1947e19717d159bb6dd7c2babf139bf5e9309e7
Sergey Savinkin <savinoldo@gmail.com>
2022-03-07 15:16:27 +0300

    ex2 task-04 trying to solve the problem of new line at the end of file

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -1,13 +1,11 @@
 function sameElements(arr) {
-    for (let i = 0; i < arr.length; i += 1) {
-      if (arr[i] !== arr[0]) {
-        return false;
-      }
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] !== arr[0]) {
+      return false;
     }
-   
-    return true;
   }
-   
-  module.exports = sameElements();
-   
-  
+
+  return true;
+}
+
+module.exports = sameElements();
