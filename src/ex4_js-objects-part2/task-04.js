6ceb3bdb4e4ae85ad5a387e9e8b7ea9e9be3633f
NikitaSukhovEpam <100701982+NikitaSukhovEpam@users.noreply.github.com>
2022-03-08 21:14:06 +0300

    First task

---
+++ src/ex4_js-objects-part2/task-04.js
@@ -0,0 +1,12 @@
+function sameElements(array) {
+    let Value
+    for (let index = 0; index < array.length-1; index++) {
+        if (array[index] === array[index+1]) {
+            Value = true;            
+        } else { Value = false; 
+            break;
+        }
+    }
+    return Value;
+}
+module.exports = checkingIdenticalElements;
