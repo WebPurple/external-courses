0da8215a0e15b42a12635a3004eca58482aaa3b3
Sergey Savinkin <savinoldo@gmail.com>
2022-03-11 18:54:20 +0300

    ex2 task-01 final version

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,11 +1,16 @@
 function typeCheck(a) {
   if (isNaN(a) === true && typeof a !== 'string') {
     return undefined;
-  } if (typeof a === 'string') {
+  }
+
+  if (typeof a === 'string') {
     return 'string';
-  } if (typeof a === 'number') {
+  }
+
+  if (typeof a === 'number') {
     return 'number';
   }
+
   return undefined;
 }
 
