ce55ada124bc0238712d9495c2437a29f412b6d8
Антон Швец <a.shvec@santehnika-tut.ru>
2022-03-15 15:32:57 +0300

    Внесены правки по замечаниям

--- src/ex3_js-objects-part1/task-06.js
+++ src/ex3_js-objects-part1/task-06.js
@@ -1,20 +1,14 @@
 function getProperty(obj, propertyPath) {
-  let message = true;
   let result = obj;
   const arr = propertyPath.split('');
   let i = 0;
   while (i < arr.length) {
-    if (result.hasOwnProperty(arr[i])) {
-      message = true;
-      result = result[arr[i]];
-    } else {
-      message = false;
+    if (!result.hasOwnProperty(arr[i])) {
+      return 'undefined';
     }
-
+    result = result[arr[i]];
     i += 1;
   }
-  if (message) {
-    return result;
-  } return 'undefined';
+  return result;
 }
-module.export = getProperty();
+module.export = getProperty;
