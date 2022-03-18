5e0c63ac46f7247895ff9b6870d32078a050b5f9
Антон Швец <a.shvec@santehnika-tut.ru>
2022-03-18 13:41:03 +0300

    Корректировка задач

--- src/ex4_js-objects-part2/task-09.js
+++ src/ex4_js-objects-part2/task-09.js
@@ -1,7 +1,8 @@
 function pastWord(str, word, num) {
   const arr = str.split(' ');
   arr.splice(num + 1, 0, word);
-  return arr;
+
+  return arr.join(' ');
 }
 
 module.export = pastWord;
--- src/ex4_js-objects-part2/task-12.js
+++ src/ex4_js-objects-part2/task-12.js
@@ -1,5 +1,5 @@
 function sumNumber(num1, num2) {
   const result = num1 + num2;
-  console.log(result.toFixed(3));
+  return (result.toFixed(3));
 }
 module.export = sumNumber;
